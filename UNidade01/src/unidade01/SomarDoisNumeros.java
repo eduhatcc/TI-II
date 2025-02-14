efa351bfd6c1e8b058064c5b7b344086db31e622
eduhatcc <eduardolwk@gmail.com>
2025-02-14 17:47:14 -0300

    Adição de laço

--- UNidade01/src/unidade01/SomarDoisNumeros.java
+++ UNidade01/src/unidade01/SomarDoisNumeros.java
@@ -6,18 +6,35 @@ public class SomarDoisNumeros {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		boolean fim = false;
+		String resp;
 		int n1 = 0,
 			n2 = 0,
 			soma = 0;
 		
-		System.out.printf("Digite o primeiro número: ");
-		n1 = sc.nextInt();
-		System.out.printf("%nDigite o segundo número: ");
-		n2 = sc.nextInt();
+		do {
+			System.out.printf("%nDigite o primeiro número: ");
+			n1 = sc.nextInt();
+			System.out.printf("%nDigite o segundo número: ");
+			n2 = sc.nextInt();
 		
-		soma = n1 + n2;
+			soma = n1 + n2;
 		
-		System.out.printf("%n%n%d + %d = %d", n1, n2, soma);
+			System.out.printf("%n%d + %d = %d%n", n1, n2, soma);
+			
+			System.out.printf("%nDeseja finalizar o programa? [S/N]: ");
+			resp = sc.next();
+			
+			if (resp.equals("s")) {
+				fim = true;
+			}
+			else {
+				n1 = 0;
+				n2 = 0;
+				soma = 0;
+			}
+		}
+		while(!fim);
 		
 		sc.close();
 	}
